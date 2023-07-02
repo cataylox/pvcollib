c9d361e296bbe4f0906deb098a60a87782ed7b64
Clint Taylor <clinton.a.taylor@intel.com>
2023-07-01 19:33:56 -0700

    Cleanup SGM code

--- coleco-examples/sgm/sgmtest/sgmtest.c
+++ coleco-examples/sgm/sgmtest/sgmtest.c
@@ -18,7 +18,7 @@ void show_ay8910(void) {
 	char i = 0; 
 	score_t score = {0,0};
 
-	for (i = 0; i < 16 ; i++) {
+	for (i = R0; i <= R15 ; i++) {
 		sys_sgm_aycntl = i;
 		sys_sgm_get_ay8910();
 		score.lo = i;
@@ -36,14 +36,14 @@ void main (void) {
 
 	// Put screen in text mode 2
 	vdp_setmode2txt();
-	
+
 	// Put default char in VRAM and duplicate to all areas
 	//  as we are going to write to line 8, it is in the second area
 	vdp_setdefaultchar(FNTNORMAL);
 	vdp_duplicatevram();
 	vdp_fillvram(0x2000,0xf1,128*8); 	// Change color (or we will see nothing)
 	vdp_enablescr();
-	
+
     // Check now if we were ok with SGM initialization
     if (sys_sgmok==1) {
         // Print text as we are not going to do something else
@@ -52,53 +52,52 @@ void main (void) {
         // Print text as we are not going to do something else
 		vdp_putstring(8,5,"NO SGM SUPPORT...");
 	}
-	
+
 	// Wait for nothing :P
-	i = 0;
 	while(1) {
 		// Wait Vblank
 		vdp_waitvblank(1);
 		if (sys_sgmok==1) {
 			if (i==0) {
 				vdp_putstring(4,3,"AY-3-8910 registers");
-				sys_sgm_set_ay8910(8, 12);
-				sys_sgm_set_ay8910(9, 12);
-				sys_sgm_set_ay8910(10, 12);
-				sys_sgm_set_ay8910(7, 0x38);
+				sys_sgm_set_ay8910(R8, 12);
+				sys_sgm_set_ay8910(R9, 12);
+				sys_sgm_set_ay8910(R10, 12);
+				sys_sgm_set_ay8910(R7, 0x38);
 			}
-			
+
 			if (delay++ < QNOTE) {
 				if (delay > (QNOTE-2)) {
-					sys_sgm_set_ay8910(0, 0);
-					sys_sgm_set_ay8910(1, 0);
-					sys_sgm_set_ay8910(2, 0);
-					sys_sgm_set_ay8910(3, 0);
-					sys_sgm_set_ay8910(4, 0);
-					sys_sgm_set_ay8910(5, 0);
+					sys_sgm_set_ay8910(R0, 0);
+					sys_sgm_set_ay8910(R1, 0);
+					sys_sgm_set_ay8910(R2, 0);
+					sys_sgm_set_ay8910(R3, 0);
+					sys_sgm_set_ay8910(R4, 0);
+					sys_sgm_set_ay8910(R5, 0);
 				} 
 				continue;
 			}
 
-			if(main_song_a[i].low == 0) {
-				sys_sgm_set_ay8910(7, 0x3f);
-				sys_sgm_set_ay8910(8, 0);
-				sys_sgm_set_ay8910(9, 0);
-				sys_sgm_set_ay8910(10, 0);
+			if(main_song_a[i].fine == 0) {
+				sys_sgm_set_ay8910(R7, 0x3f);
+				sys_sgm_set_ay8910(R8, 0);
+				sys_sgm_set_ay8910(R9, 0);
+				sys_sgm_set_ay8910(R10, 0);
 				show_ay8910();
 				if (joypad_1 & PAD_RIGHT)
-					i=0;
+					i=0; // Restart song
 			} else {
-				if (main_song_a[i].high != 255) {	
-					sys_sgm_set_ay8910(0, main_song_a[i].low);
-					sys_sgm_set_ay8910(1, main_song_a[i].high);
+				if (main_song_a[i].course != 255) {	
+					sys_sgm_set_ay8910(R0, main_song_a[i].fine);
+					sys_sgm_set_ay8910(R1, main_song_a[i].course);
 				}
-				if (main_song_b[i].high != 255) {	
-					sys_sgm_set_ay8910(2, main_song_b[i].low);
-					sys_sgm_set_ay8910(3, main_song_b[i].high);
+				if (main_song_b[i].course != 255) {	
+					sys_sgm_set_ay8910(R2, main_song_b[i].fine);
+					sys_sgm_set_ay8910(R3, main_song_b[i].course);
 				}
-				if (main_song_c[i].high != 255) {	
-					sys_sgm_set_ay8910(4, main_song_c[i].low);
-					sys_sgm_set_ay8910(5, main_song_c[i].high);
+				if (main_song_c[i].course != 255) {	
+					sys_sgm_set_ay8910(R4, main_song_c[i].fine);
+					sys_sgm_set_ay8910(R5, main_song_c[i].course);
 				}
 				show_ay8910();
 				i++;
--- pvcollib/include/coleco/sgm.h
+++ pvcollib/include/coleco/sgm.h
@@ -38,147 +38,6 @@
 
 #include <coleco/coltypes.h>
 
-
-#define QNOTE 13
-
-struct notes {
-	unsigned char low;
-	unsigned char high;
-};
-/*
-08E3: 4E 05   ;   82.36 2E
-08E5: 01 05   ;   87.31 2F
-08E7: B9 04   ;   92.51 2F#
-08E9: 76 04   ;   97.94 2G
-08EB: 36 04   ;  103.75 2G#
-08ED: F9 03   ;  109.97 2A
-08EF: C0 03   ;  116.50 2A#
-08F1: 8A 03   ;  123.45 2B
-08F3: 57 03   ;  130.81 3C
-08F5: 27 03   ;  138.59 3C#
-08F7: FA 02   ;  146.78 3D
-08F9: CF 02   ;  155.56 3D#
-08FB: A7 02   ;  164.72 3E
-08FD: 81 02   ;  174.49 3F
-08FF: 5D 02   ;  184.87 3F#
-0901: 3B 02   ;  195.88 3G
-0903: 1B 02   ;  207.51 3G#
-0905: FD 01   ;  219.74 3A
-0907: E0 01   ;  233.01 3A#
-0909: C5 01   ;  246.90 3B
-090B: AC 01   ;  261.32 4C
-090D: 94 01   ;  276.85 4C#
-090F: 7D 01   ;  293.56 4D
-0911: 68 01   ;  310.68 4D#
-0913: 53 01   ;  329.93 4E
-0915: 40 01   ;  349.52 4F
-0917: 2E 01   ;  370.35 4F#
-0919: 1D 01   ;  392.44 4G
-091B: 0D 01   ;  415.79 4G#
-091D: FE 00   ;  440.34 4A
-091F: F0 00   ;  466.03 4A#
-0921: E3 00   ;  492.72 4B
-0923: D6 00   ;  522.65 5C
-0925: CA 00   ;  553.70 5C#
-0927: BE 00   ;  588.67 5D
-0929: B4 00   ;  621.37 5D#
-092B: AA 00   ;  657.93 5E*/
-static const struct notes main_song_a[] = {
-	{.low = 0xe3, .high = 0x00}, // 4B
-	{0x1d, 0x01}, // 4G
-	{0x1d, 0x01}, // 4G
-	{0x1d, 0x01}, // 4G
- 	{0xe3, 0x00}, // 4B
-	{0x1d, 0x01}, // 4G
-	{0x1d, 0x01}, // 4G
-	{0x1d, 0x01}, // 4G
-	{0xd6, 0x00}, // 5C
-	{0xd6, 0x00}, // 5C
-	{0xe3, 0x00}, // 4B
-	{0xe3, 0x00}, // 4B
-	{0xfe, 0x00}, // 4A
-	{0xFF, 0xFF}, //rest TODO
-	{0xd6, 0x00}, // 5C
-	{0xd6, 0x00}, // 5C
-	{0xe3, 0x00}, // 4B
-	{0xe3, 0x00}, // 4B
-	{0xfe, 0x00}, // 4A
-	{0xfe, 0x00}, // 4A
-	{0xAA, 0x00}, // 5E
-	{0xAA, 0x00}, // 5E
-	{0xBE, 0x00}, // 5D
-	{0xd6, 0x00}, // 5C
-	{0xe3, 0x00}, // 4B
-	{0xfe, 0x00}, // 4A
-	{0x1d, 0x01}, // 4G
-	{0x00, 0x00},
-};
-
-static const struct notes main_song_b[] = {
-	{0x3b, 0x02}, // 3G
-	{0xc5, 0x01}, // 3B
-	{0xFA, 0x02}, // 3D
-	{0xc5, 0x01}, // 3B
-	{0x3b, 0x02}, // 3G
-	{0xc5, 0x01}, // 3B
-	{0xFA, 0x02}, // 3D
-	{0xc5, 0x01}, // 3B
-	{0xFD, 0x01}, // 3A
-	{0xAC, 0x01}, // 4C
-	{0xFA, 0x02}, // 3D
-	{0xAC, 0x01}, // 4C
-	{0xFD, 0x01}, // 3A
-	{0xAC, 0x01}, // 4C
-	{0xFA, 0x02}, // 3D
-	{0xAC, 0x01}, // 4C
-	{0xFD, 0x01}, // 3A
-	{0xAC, 0x01}, // 4C
-	{0xFA, 0x02}, // 3D
-	{0xAC, 0x01}, // 4C
-	{0xFD, 0x01}, // 3A
-	{0xAC, 0x01}, // 4C
-	{0xFA, 0x02}, // 3D
-	{0xAC, 0x01}, // 4C
-	{0xFD, 0x01}, // 3A
-	{0xAC, 0x01}, // 4C
-	{0xFA, 0x02}, // 3D
-	{0xAC, 0x01}, // 4C
-	{0xc5, 0x01}, // 3B
-	{0xFF, 0xFF}, // Reset
-	{0x00, 0x00},
-};
-
-static const struct notes main_song_c[] = {
-	{0xFF, 0xFF}, // Reset
-	{0x3b, 0x02}, // 3G
-	{0xFF, 0xFF}, // Reset
-	{0x3b, 0x02}, // 3G
-	{0xFF, 0xFF}, // Reset
-	{0x3b, 0x02}, // 3G
-	{0xFF, 0xFF}, // Reset
-	{0xFD, 0x01}, // 3A
-	{0xFF, 0xFF}, // Reset
-	{0xFD, 0x01}, // 3A
-	{0xFF, 0xFF}, // Reset
-	{0xFD, 0x01}, // 3A
-	{0xFF, 0xFF}, // Reset
-	{0xFD, 0x01}, // 3A
-	{0xFF, 0xFF}, // Reset
-	{0xFD, 0x01}, // 3A
-	{0xFF, 0xFF}, // Reset
-	{0xFD, 0x01}, // 3A
-	{0xFF, 0xFF}, // Reset
-	{0xFD, 0x01}, // 3A
-	{0xFF, 0xFF}, // Reset
-	{0xFD, 0x01}, // 3A
-	{0xFF, 0xFF}, // Reset
-	{0xFD, 0x01}, // 3A
-	{0xFF, 0xFF}, // Reset
-	{0x3b, 0x02}, // 3G
-	{0xFF, 0xFF}, // Reset
-	{0x00, 0x00},
-};
-
 /**
  *  \brief sys_sgmok is set if sgm module is present<br>
  *	when calling sys_sgminit function<br>
@@ -186,7 +45,7 @@ static const struct notes main_song_c[] = {
 extern volatile u8 sys_sgmok;
 
 /**
- *  \brief sgm_aycntl is r/w sgm module ay ctl register<br>
+ *  \brief sgm_aycntl allows reading of sgm module ay registers<br>
  *	<br>
  */
 extern volatile u8 sys_sgm_aycntl;
@@ -197,7 +56,19 @@ extern volatile u8 sys_sgm_aycntl;
  * Activate SGM and init sys_sgmok variable with 1 if it is ok<br>
 */
 void sys_sgminit(void);
+
+/**
+ * \fn void sys_get_ay8910(void)
+ * \brief Read AY-3-8910 register value<br>
+ * Read Value from register in sys_sgm_ayctrl, value returned<br>
+*/
 void sys_sgm_get_ay8910(void);
+
+/**
+ * \fn void sys_sgmset_ay8910(reg, value)
+ * \brief Set AY-3-8910 Registers to value<br>
+ * Write value to reg where reg = 0-15<br>
+*/
 void sys_sgm_set_ay8910(unsigned char, unsigned char);
 
 #endif
